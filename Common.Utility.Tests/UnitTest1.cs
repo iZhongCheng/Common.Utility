156607872b1c6d9c33b2b2f850c57e71e1eff872
imhitch@qq.com <kiss270912739>
2016-02-29 11:35:44 +0800

    添加 Common.Utility.Tests 项目

---
+++ Common.Utility.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Common.Utility.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
